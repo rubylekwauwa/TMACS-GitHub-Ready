f9eac49d510b0a1a5a068642b7186f243bb22edd
rubylekwauwa <rubylekwauwa@gmail.com>
2026-09-14 20:28:01 -0400

    Added Nedson Campbell as mentor

--- azure-entra-static-web-app/src/app/app.js
+++ azure-entra-static-web-app/src/app/app.js
@@ -43,7 +43,17 @@ email: "uche.aneni@yale.edu",
 image: "https://lirp.cdn-website.com/8130e6b2/import/clib/fhchc_org/dms3rep/multi/opt/Kammarauche-Asuzu-975x1218-1920w.jpeg",
 link: ""
   },
-
+{
+  name: "Nedson Campbell, MD",
+  city: "Philadelphia, PA",
+  coords: [-75.1652, 39.9526],
+  specialties: ["Addiction psychiatry", "Leadership and administrative psychiatry"],
+  focusAreas: ["Leadership", "Career transitions", "Work-life integration", "Identity/lived experience", "Financial literacy", "Culturally responsive care", "Family life/parenting as a psychiatrist"],
+  shortBlurb: "Psychiatrist and Chief Medical Officer with expertise in addiction psychiatry, clinical leadership, recruitment, and building a sustainable work-life balance.",
+  fullBio: "I am an adult and addiction psychiatrist who now works primarily as a healthcare administrator, serving as Chief Medical Officer for a group of outpatient clinics. Our clinics provide care for children and adolescents, individuals with eating disorders and addictions, and people with a broad range of mental health needs. In addition to developing policies and procedures, I provide clinical coverage across these specialty areas for approximately half of the year.<br><br>I am especially interested in mentoring residents around leadership, recruitment, career transitions, financial literacy, culturally responsive care, and balancing professional and personal responsibilities. Outside of work, I am a married father of two, an avid Brazilian jiu-jitsu practitioner, and a regular at the gym.",
+  availability: "TBD"
+  link: "https://www.linkedin.com/in/nedson-campbell-9b7787106/"
+}
 {
     name: "Gwendolyn Lopez-Cohen, MD",
     city: "Westport, CT",
